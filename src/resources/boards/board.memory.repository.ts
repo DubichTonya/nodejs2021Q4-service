8d40e7ec0654f1b2fbc5c82bbbd9bf682f72b9d9
Antonina Dubich <dubich.t@gmail.com>
2021-12-10 23:56:51 +0300

    feat: add ts docs in board.memory.repository

--- src/resources/boards/board.memory.repository.ts
+++ src/resources/boards/board.memory.repository.ts
@@ -13,29 +13,61 @@ interface IBoards {
 
 const boardData: IBoards[] = [];
 
+/**
+ * Returns all boards
+ * @returns array with boards objects
+ */
+
 function getBoardData(): IBoards[] {
   return boardData;
 }
 
+/**
+ * Returns noting. Adding board in array of boards
+ * @param board - board object
+ */
+
 function addBoard(board: IBoards): void {
-  boardData.push(board)
+  boardData.push(board);
 }
+
+/**
+ * Returns noting. Deleting board from array of boards
+ * @param boardIndex - index of board (number)
+ */
 
 function deleteBoardFromData(boardIndex: number): void {
   boardData.splice(boardIndex, 1);
 }
 
-function updateBoardInData(boardIndex: number, req: FastifyRequest) {
+/**
+ * Returns noting. Updating board from array of boards
+ * @param boardIndex - index of board (number)
+ * @param req - request from server (FastifyRequest)
+ */
+
+function updateBoardInData(boardIndex: number, req: FastifyRequest): void {
   const body = <IBoards>req.body;
   boardData[boardIndex] = { ...boardData[boardIndex], ...body };
 }
 
+/**
+ * Search board by id in array of boards
+ * @param boardId - id of board (string)
+ * @returns return board object or undefined if not found board
+ */
 
-function findBoardById(boardId: string) {
+function findBoardById(boardId: string): IBoards | undefined {
   return boardData.find(item => item.id === boardId);
 }
 
-function findBoardByIndex(boardId: string) {
+/**
+ * Search board index by id in array of boards
+ * @param boardId - id of board (string)
+ * @returns return board index (number) or -1 if not found board
+ */
+
+function findBoardByIndex(boardId: string): number {
   return boardData.findIndex(item => item.id === boardId);
 }
 

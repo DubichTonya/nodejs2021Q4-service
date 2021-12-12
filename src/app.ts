5b9dac7bb01d38c96d97c7b286e44244017ccb23
Antonina Dubich <dubich.t@gmail.com>
2021-12-12 14:25:29 +0300

    fix: change extension on ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 export {}
 const fastify = require('fastify')({ logger: false });
-const { PORT } = require('./common/config');
+const { PORT } = require('./common/config.ts');
 const { userRoutes } = require('./resources/users/user.router.ts');
 const { boardRoutes } = require('./resources/boards/board.router.ts');
 const { taskRoutes } = require('./resources/tasks/task.router.ts');

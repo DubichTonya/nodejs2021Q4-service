ebf4fa697995dc1c13beb586aae3d54c3fa99509
Antonina Dubich <dubich.t@gmail.com>
2022-01-16 15:31:41 +0300

    feat: add settings

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,1 @@
-FROM node:16-alpine
-
-WORKDIR /app
-COPY package*.json .
-RUN npm install && rm -rf /var/lib/apt/lists/*
-COPY . .
-EXPOSE 4000
-CMD ["npm", "run", "dev"]
+FROM postgres:alpine
---
+++ src/db.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata";
+import { createConnection } from 'typeorm';
+
+export const connection = createConnection();
+
+connection.then(async (c) => {
+  await c.runMigrations();
+}).catch((error) => {
+  console.log(error);
+})
+

28887f55ed7a294f825e48f325e2536bcb0c9ae6
Antonina Dubich <dubich.t@gmail.com>
2022-02-03 22:42:01 +0300

    feat: adds enviroment variable USE_FASTIFY

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,10 +1,17 @@
 import * as dotenv from 'dotenv';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || '0.0.0.0';
 const JWT_SECRET_KEY = <string>process.env.JWT_SECRET_KEY;
-// const { AUTH_MODE } =  process.env.AUTH_MODE === 'true';
 
-export { PORT, HOST, JWT_SECRET_KEY };
+const USE_FASTIFY = () => {
+  if (process.env.USE_FASTIFY === 'true') {
+    return new FastifyAdapter();
+  }
+};
+
+export { PORT, HOST, JWT_SECRET_KEY, USE_FASTIFY };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PORT } from './common/config';
+import { PORT, USE_FASTIFY } from "./common/config";
 import { AuthGuard } from './guards/auth.guard';
 import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    USE_FASTIFY()
+  );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalGuards(new AuthGuard());
   await app.listen(PORT);

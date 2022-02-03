cc9ece31c2d9855effc008ca5ecec1ca46ff482c
Antonina Dubich <dubich.t@gmail.com>
2022-02-04 00:17:29 +0300

    fix: problems with file upload

--- src/common/checkToken.ts
+++ src/common/checkToken.ts
@@ -3,7 +3,7 @@ import { JWT_SECRET_KEY } from './config';
 const jwt = require('jsonwebtoken');
 
 export function checkToken(req): boolean {
-  if (!(req.url === '/' || req.url === '/doc' || req.url === '/login')) {
+  if (!(req.url === '/' || req.url === '/doc' || req.url === '/login' || req.url.includes('file'))) {
     const authorization = req.headers?.authorization;
     if (!authorization) {
       return false;
